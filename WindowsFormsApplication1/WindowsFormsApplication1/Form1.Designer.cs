5ffaa37fdaa8c2ca0bba1975f4dee23c9e8cada9
LOCCIONI\l.scortichini <l.scortichini@loccioni.com>
2018-05-04 17:17:10 +0200

    prova1

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 108);
+            this.button1.Location = new System.Drawing.Point(97, 121);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(150, 94);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
